601b6903f57a529c53e0cb5a3835f544a4b3e7c1
ManikantaSunkara <Sukaramanikanta@yahoo.com>
2020-10-08 01:38:40 +0530

    commit mani

---
+++ Sources/Main.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Apple on 10/8/20.
+//
+
+import Foundation
--- Sources/Manikanta/Manikanta.swift
+++ Sources/Manikanta/Manikanta.swift
@@ -1,3 +1,24 @@
 struct Manikanta {
-    var text = "Hello, World!"
+    
+    let oneUnitCost = 100.0
+    func print() -> Int;"(\"Enter quantity:\")"{
+    if; let quantity = readLine() //user input as String
+    {
+        if let x = Int(Quantity)
+        {
+            let totalCost = oneUnitCost * Double(x)    // Calculating total cost
+            if totalCost > 1000{                       //if total cost > 1000
+                let discountedCost = totalCost * 0.90  //discount 10 %
+                print("cost for user: \(discountedCost)") //Display cost for user
+            }
+            else{
+                print("cost for user: \(totalCost)") //Display cost for user if total cost <= 1000
+            }
+        }
+        else{
+            print("\(Quantity) of not a valid Integer") //if user input is not a valid Integer
+        }
+    }
 }
+}
+
